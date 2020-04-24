53235ca84e683c8f7bb076547bb05e9f054c4439
PATHAN <owaiskamal12@gmail.com>
2020-04-24 14:05:30 +0500

    firebase-config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +18,11 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAo3IE0qXpaUi4eA4Idswgll0lpEr0PWII",
+    authDomain: "i18n-manager-9c3e3.firebaseapp.com",
+    databaseURL: "https://i18n-manager-9c3e3.firebaseio.com",
+    projectId: "i18n-manager-9c3e3",
+    storageBucket: "i18n-manager-9c3e3.appspot.com",
+    messagingSenderId: "311419019449",
+    appId: "1:311419019449:web:ef7dfbb840ed7477027b68",
+    measurementId: "G-1FVMGPY6KQ"
+  }
 };
 
 /*
