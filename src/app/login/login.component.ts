a83b6bdb218f67b840fef21475cd1cfe06114291
PATHAN <owaiskamal12@gmail.com>
2020-04-24 20:05:56 +0500

    auth-database

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,28 +7,38 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
 
   constructor(public authService : AuthService) { }
-=======
   
   user = {
     email: '',
     password: '',
   }
-  constructor() { }
->>>>>>> bcb79ba04dbbc73d697c4664412df8f28bd2c8a8
+  
 
   ngOnInit(): void {
 
    
   }
 
-  login()
-  {
-    
-  }
+  
   onSubmit(){
-    console.log("User", this.user);
+    this.authService.login(this.user.email , this.user.password)
+    if(this.authService.user)
+    {
+      console.log("hello");
+      
+    }
+    else{
+      console.log("no hello");
+      
+    }
+
+  
+  }
+
+  logout()
+  {
+    this.authService.logout();
   }
 }
