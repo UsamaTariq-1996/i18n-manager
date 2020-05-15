62f868d3309be9a066a6769fc3ce03b898e7f5c8
PATHAN <owaiskamal12@gmail.com>
2020-05-16 03:40:53 +0500

    all-committed

--- src/app/navbars/navbars.component.ts
+++ src/app/navbars/navbars.component.ts
@@ -12,14 +12,20 @@ import { TreeService } from "../services/tree.service";
 import { MatTreeNestedDataSource } from "@angular/material/tree";
 import { NestedTreeControl } from "@angular/cdk/tree";
 import { ArrayType } from "@angular/compiler";
-import * as _ from 'lodash';
-import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
-import { AuthService } from '../auth.service';
-import { deepEqual } from 'assert';
-import * as deepmerge from 'deepmerge';
-import { map, retry } from 'rxjs/operators';
-import { LanguagesService, GoogleObj } from '../services/languages.service';
-
+import * as _ from "lodash";
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { AuthService } from "../auth.service";
+import { deepEqual } from "assert";
+import * as deepmerge from "deepmerge";
+import { map, retry } from "rxjs/operators";
+import { LanguagesService, GoogleObj } from "../services/languages.service";
+import { element } from 'protractor';
 
 export class FileNode {
   children: FileNode[];
@@ -29,11 +35,10 @@ export class FileNode {
 
 var TREE_DATA: string;
 
-
 @Injectable()
 export class FileDatabase {
   dataChange = new BehaviorSubject<FileNode[]>([]);
-  cross : any[] = [];
+  cross: any[] = [];
 
   get data(): FileNode[] {
     return this.dataChange.value;
@@ -48,35 +53,29 @@ export class FileDatabase {
     setTimeout(() => {
       const dataObject = JSON.parse(TREE_DATA);
 
-      
-      
       // for(var i = 0 ; i < dataObject.length ; i++)
       // {
       //   var lengths =  Object.assign({}, ...function _flatten(o) { return [].concat(...Object.keys(o).map(k => typeof o[k] === 'object' ? _flatten(o[k]) : ({[k]: o[k]})))}(dataObject[i]))
       //    this.cross.push(Object.keys(lengths).length)
-        
+
       //   if(this.cross[i-1] != null)
-        
-        
+
       //   {
       //   if(this.cross[i] < this.cross[i-1])
       //   {
-          
+
       //       var obj = dataObject[i-1]
       //   }
       //   else{
       //     console.log("else oste");
-          
+
       //     obj = dataObject[i]
       //   }
       // }
-          
-      
+
       // }
 
-//      console.log(obj , "SORTED ");
-          
-  
+      //      console.log(obj , "SORTED ");
 
       // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
       //     file node as children.
@@ -96,14 +95,11 @@ export class FileDatabase {
       const value = obj[key];
       const node = new FileNode();
       node.filename = key;
-      
 
       if (value != null) {
         if (typeof value === "object") {
-         
-            console.log(key , "+1");
-            
-          
+          console.log(key, "+1");
+
           node.children = this.buildFileTree(value, level + 1);
         } else {
           node.type = value;
@@ -129,32 +125,33 @@ export class NavbarsComponent implements OnInit {
   showcontent: boolean = false;
   dynamicForm: FormGroup;
   submitted = false;
- 
+
   public headers: any[] = [];
-  colorControl = new FormControl('accent');
+  colorControl = new FormControl("accent");
   imageDetailList: AngularFireList<any>;
   types = [
     { value: "all", viewValue: "All" },
     { value: "some", viewValue: "Some" },
     { value: "none", viewValue: "None" },
   ];
-  count : number = 0;
+  count: number = 0;
   downloadURL: Observable<any>;
   JsonArray: any;
   jsonurl: any;
   temp: any[] = [];
-  userTemp:any[] = [];
-  nestsort : any[] = []
+  userTemp: any[] = [];
+  nestsort: any[] = [];
   nestedTreeControl: NestedTreeControl<FileNode>;
   nestedDataSource: MatTreeNestedDataSource<FileNode>;
   data_tree: any[] = [];
   temp1: any[] = [];
-  
-  todo: Observable<{ id: string; }[]>;
-  userTodo: Observable<{ id: string; }[]>;
-  getnode : any;
-  langArr: any;
+   checkarr :any[] = [];
+  todo: Observable<{ id: string }[]>;
+  userTodo: Observable<{ id: string }[]>;
+  getnode: any;
+  langArr: any[] = [];
   upData: object;
+  tempo: any[] = [];
   constructor(
     database: FileDatabase,
     private router: Router,
@@ -163,248 +160,279 @@ export class NavbarsComponent implements OnInit {
     private treeService: TreeService,
     private formBuilder: FormBuilder,
     public authService: AuthService,
-    private langService : LanguagesService
-    
+    private langService: LanguagesService
   ) {
-   
     this.count = 0;
     this.todoCollectionRef = this.afs.collection<any>("localization");
-    console.log(this.todoCollectionRef , "mi amor");
-    console.log(this.userCollectionRef , "user collection ref");
+
     this.todo$ = this.todoCollectionRef.valueChanges();
-    this.todo = this.todoCollectionRef.snapshotChanges().pipe(map(actions =>
-      actions.map(a =>{
-        const id = a.payload.doc.id;
-        return {id}
-      })));
-
-      console.log(this.todo);
-     
-        console.log(this.userTodo,"User todo");  
-        if(authService.isLoggedIn){
-          
-          var userArr = localStorage.getItem('uid');
-          console.log(userArr,"user uid check")
-          var userCollectionRef = this.afs.collection<any>('users').doc(userArr);
-
-          userCollectionRef.get().subscribe(function(doc) {
-            if (doc.exists) {
-                 this.langArr = doc.data();
-                console.log("Document data:", this.langArr);
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
+    this.todo = this.todoCollectionRef.snapshotChanges().pipe(
+      map((actions) =>
+        actions.map((a) => {
+          const id = a.payload.doc.id;
+          return { id };
         })
-      }
-     
+      )
+    );
+
+    console.log(this.todo);
+
+    console.log(this.userTodo, "User todo");
+
     this.nestedTreeControl = new NestedTreeControl<FileNode>(this._getChildren);
     this.nestedDataSource = new MatTreeNestedDataSource();
 
     database.dataChange.subscribe(
       (data) => (this.nestedDataSource.data = data)
     );
-    
-    this.todo.subscribe((res) =>{
-      res.forEach(element => {
-        this.temp.push(element)
+
+    this.todo.subscribe((res) => {
+      res.forEach((element) => {
+        this.temp.push(element);
       });
-      
-    })
-    
-  
-  
-     
+    });
+
     this.todo$.subscribe((res) => {
       // res.forEach((element) => {
-        
-        
-        this.data_tree =res;
-        res.forEach(element => {
-         
-        });
 
-        var te = deepmerge.all(this.data_tree);
-        
-       this.upData = te; 
-      // }); 
+      this.data_tree = res;
+      res.forEach((element) => {});
 
-      
+      var te = deepmerge.all(this.data_tree);
 
-       //The depth level specifying how deep a nested array structure should be flattened. Defaults to 1.
-        
-      
+      this.upData = te;
+      // });
+
+      //The depth level specifying how deep a nested array structure should be flattened. Defaults to 1.
+
       TREE_DATA = JSON.stringify(te);
-      console.log(TREE_DATA , "new log");
-      
-      
+      console.log(TREE_DATA, "new log");
+      console.log(this.langArr, "auth2");
     });
   }
-  
-  
+
   ngOnInit(): void {
+    if (this.authService.isLoggedIn) {
+      var userArr = localStorage.getItem("uid");
+      console.log(userArr, "user uid check");
+      var userCollectionRef = this.afs.collection<any>("users").doc(userArr);
+
+      userCollectionRef.get().subscribe((doc) => {
+        if (doc.exists) {
+          this.langArr.push(doc.data());
+          console.log("Document data:", this.langArr);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      });
+    }
     //  this.getUrlData()
     this.dynamicForm = this.formBuilder.group({
-      tickets: new FormArray([])
-  });
+      tickets: new FormArray([]),
+    });
   }
-  get f() { return this.dynamicForm.controls; }
-  get t() { return this.f.tickets as FormArray; }
+  get f() {
+    return this.dynamicForm.controls;
+  }
+  get t() {
+    return this.f.tickets as FormArray;
+  }
 
   logout() {
     this.authService.logout();
-    this.router.navigateByUrl('/login')
+    this.router.navigateByUrl("/login");
   }
-  login(){
-    this.router.navigateByUrl('/login')
+  login() {
+    this.router.navigateByUrl("/login");
   }
 
   itemClick(node) {
-    console.log(node , "events");
-    console.log(localStorage.getItem('user') , "main");
-    
+    console.log(node, "events");
+    console.log(this.langArr[0].edit_right, "authentication");
+    var rights = this.langArr[0].edit_right;
     this.nestsort = [];
     console.log(node.type, "game on ha");
     this.showcontent = true;
-    this.t.clear()
-      var i = 0
-      var aisa = node.filename
-      this.getnode = node.filename;
-      console.log(this.t.value, "asdasd");
-      
-      
+    this.t.clear();
+    var i = 0;
+    var aisa = node.filename;
+    this.getnode = node.filename;
+    console.log(this.t.value, "asdasd");
+    var check;
     
-    
-         for (let i = 0 ; i < this.data_tree.length; i++) {
-             this.nestsort.push(Object.assign({}, ...function _flatten(o) { return [].concat(...Object.keys(o).map(k => typeof o[k] === 'object' ? _flatten(o[k]) : ({[k]: o[k]})))}(this.data_tree[i])))
-       
-                       
-                     this.t.push(this.formBuilder.group({
-                     name: [this.nestsort[i][aisa]]  
-             }));
-             /* console.log(this.t.value,"hhadjakdaslda"); */
-             this.temp1 = this.t.value;
-             console.log(this.nestsort[i][aisa],"temp111");
-             
-         }
   
-        
-    } 
-
-    onSubmit(event)
-    {
-      var nesteds = null ;
-     console.log(event , 'submitvalues');
-     this.temp1 = event.tickets
-    //  var i = 0;
-      
-      var str = this.temp1[0].name;
-      console.log(this.upData , "main");
-   //nesteds =  this.findPath(str , this.upData);
-   for(let j = 0 ; j < this.temp1.length ; j++)
-   {
-    if(this.temp1[j] != null  && nesteds == null )
-    {
-      nesteds = this.findPath(this.temp1[j].name , this.upData)
-    }
-
-   }
-    console.log(nesteds   , "finder");
-    
-      for(let i = 0 ; i < this.data_tree.length ; i++)
-      {
-        
-        if(nesteds != "" || nesteds != null)
-        {
-          if(nesteds.length == 1)
-          {
-            var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
-              [this.getnode] :  this.temp1[i].name 
-              })
-          }
-          else if (nesteds.length == 2)
-          {
-            var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
-              [`${nesteds[0]}.${nesteds[1]}`] :  this.temp1[i].name 
-              })
-          }
-          else if (nesteds.length == 3)
-          {
-            var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
-              [`${nesteds[0]}.${nesteds[1]}.${nesteds[2]}`] :  this.temp1[i].name 
-              })
-          }
-          else if (nesteds.length == 4)
-          {
-            var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
-              [`${nesteds[0]}.${nesteds[1]}.${nesteds[2]}.${nesteds[3]}`] :  this.temp1[i].name 
-              })
-          } 
-          
+    for (let i = 0; i < this.data_tree.length; i++) {
+      this.nestsort.push(
+        Object.assign(
+          {},
+          ...(function _flatten(o) {
+            return [].concat(
+              ...Object.keys(o).map((k) =>
+                typeof o[k] === "object" ? _flatten(o[k]) : { [k]: o[k] }
+              )
+            );
+          })(this.data_tree[i])
+        )
+      );
+ 
+      var user;
+     
+        user = this.temp[i].id;
+        var check
+        if (rights.indexOf(user) >= 0) {
+        check = false;
+          console.log(user + ' is a good user');
+        } else {
+          console.log(user + ' is BAD!!!');
+          check= true
         }
-        else{
-
-      console.log("multiple errors");
       
-    }
-       }
-    }
+      // rights.forEach((element) => {
+      //   console.log(element);
+        
+      //   if (element == this.temp[i].id) {
+      //     check = false;
+         
+      //     console.log(element , "is equal to " , this.temp[i].id);
+          
+  
+      //   }
+        
+      //    else if (element != this.temp[i].id) {
+      //     check = true;
+         
+      //     console.log(element , "is not to " , this.temp[i].id);
+      //   }
+      //   else{
+      //     check = true
+      //   }
 
-     findPath(a, obj) {
-      function iter(o, p) {
-          return Object.keys(o).some(function (k) {
-              result = p.concat(Array.isArray(o) ? +k : k);
-              return o[k] === a || o[k] && typeof o[k] === 'object' && iter(o[k], result);
-          });
-      }
-      var result;
-      return iter(obj, []) && result || undefined;
+        
+      // });
+
+
+
+      
+      this.t.push(
+        this.formBuilder.group({
+          name: { value: [this.nestsort[i][aisa]], disabled: check },
+        })
+      );
+      
+      
+
+      /* console.log(this.t.value,"hhadjakdaslda"); */
+     this.temp1 = this.t.value;
+      //console.log(this.temp1, "temp111");
+    }
   }
 
 
+  onSubmit(event) {
+    var nesteds = null;
+  
+    this.temp1 = event.tickets;
+    //  var i = 0;
+    console.log(this.temp1, "submitvalues");
+    //var str = this.temp1[2].name;
+    console.log(this.upData, "main");
+    const result = this.temp1.reduce((r, obj) => r.concat(obj.name), []);
+   // nesteds =  this.findDepth(str , this.upData);
+    for (let j = 0; j < this.temp1.length; j++) {
+      if (this.temp1[j] != null && nesteds == null) {
+        nesteds = this.findPath(result[j], this.upData) ;
+      
+      }
+    }
    
+    console.log(nesteds, "finder");
+    console.log(result , "kkkk");
         
-    myname(event)
-    {
-      //console.log(event , this.temp1[event2].name , "lnag");
-     // var sourcelang = this.temp[event2].id;
-      var targetlang = this.temp[event].id;
-      
-      //var sourceex = /[^-]*$/g;
-      var targetex = /[^-]*$/g;
-      //var sourceresult = sourceex.exec(sourcelang);
-      var targetresult = targetex.exec(targetlang);
-      console.log(targetresult , "maro");
-      
+
+    for (let i = 0; i < this.data_tree.length; i++) {
+      if (nesteds != "" || nesteds != null) {
+        if(result[i] == null)
+        {
+          result[i] = ""
+        }
+        if (nesteds.length == 1) {
+          var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
+            [this.getnode]: result[i],
+          });
+        } else if (nesteds.length == 2) {
+          var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
+            [`${nesteds[0]}.${nesteds[1]}`]: result[i],
+          });
+        } else if (nesteds.length == 3) {
+          var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
+            [`${nesteds[0]}.${nesteds[1]}.${nesteds[2]}`]: result[i],
+          });
+        } else if (nesteds.length == 4) {
+          var updateRef = this.todoCollectionRef.doc(this.temp[i].id).update({
+            [`${nesteds[0]}.${nesteds[1]}.${nesteds[2]}.${nesteds[3]}`]: result[i],
+          });
+        }
+      } else {
+        console.log("multiple errors");
+      }
+    }
+  }
+   flatten(ary) {
+    var ret = [];
+    for(var i = 0; i < ary.length; i++) {
+        if(Array.isArray(ary[i])) {
+            ret = ret.concat(this.flatten(ary[i]));
+        } else {
+            ret.push(ary[i]);
+        }
+    }
+    return ret;
+}
+
+   findPath(a, obj) {
+    function iter(o, p) {
+        return Object.keys(o).some(function (k) {
+            result = p.concat(Array.isArray(o) ? +k : k);
+            return o[k] === a || o[k] && typeof o[k] === 'object' && iter(o[k], result);
+        });
+    }
+    var result;
+    return iter(obj, []) && result || undefined;
+  }
+
+ 
+  myname(event) {
+    //console.log(event , this.temp1[event2].name , "lnag");
+    // var sourcelang = this.temp[event2].id;
+    var targetlang = this.temp[event].id;
+
+    //var sourceex = /[^-]*$/g;
+    var targetex = /[^-]*$/g;
+    //var sourceresult = sourceex.exec(sourcelang);
+    var targetresult = targetex.exec(targetlang);
+    console.log(targetresult, "maro");
+
+    // console.log(ex , "asdasd");
+    var result = this.temp1.reduce((r, obj) => r.concat(obj.name), []);
+    console.log(result , "really");
     
-     // console.log(ex , "asdasd");
-      
-      const googleObj: GoogleObj = {
-        q: this.temp1[0].name,
-        target: targetresult[0] ,
-        source: 'en' , 
-        };
-      this.langService.translate(googleObj).subscribe(res => {
-        console.log(res);
-         
+    const googleObj: GoogleObj = {
+      q: result[0],
+      target: targetresult[0],
+      source: "en",
+    };
+    this.langService.translate(googleObj).subscribe((res) => {
+      console.log(res);
+
       var str = res.data.translations[0].translatedText;
-      this.t.at(event).get('name').patchValue(str)
+      this.t.at(event).get("name").patchValue(str);
       this.temp1 = [];
       this.temp1 = this.t.value;
-      console.log(this.temp1 , "temper");
-      
-          })
+      console.log(this.temp1, "temper");
+    });
+  }
 
-
-          
-
-
-    }
-        
-  
-    
-  
- 
   //  getUrlData()
   // {
 
